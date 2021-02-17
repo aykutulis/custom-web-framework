1ee1f3ea7c84acbcf04218f1ec0f6ce36ea4f870
Aykut Ulış <aykutulis@hotmail.com>
2021-02-18 02:51:21 +0300

    Generic Collection class

--- src/models/Collection.ts
+++ src/models/Collection.ts
@@ -1,12 +1,11 @@
 import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
-import { User, UserProps } from './User';
 
-export class Collection {
-  models: User[] = [];
+export class Collection<T, K> {
+  models: T[] = [];
   events: Eventing = new Eventing();
 
-  constructor(public rootUrl: string) {}
+  constructor(public rootUrl: string, public deserialize: (json: K) => T) {}
 
   get on() {
     return this.events.on;
@@ -18,9 +17,8 @@ export class Collection {
 
   fetch(): void {
     axios.get(this.rootUrl).then((res: AxiosResponse) => {
-      res.data.forEach((value: UserProps) => {
-        const user = User.buildUser(value);
-        this.models.push(user);
+      res.data.forEach((value: K) => {
+        this.models.push(this.deserialize(value));
       });
       this.trigger('change');
     });
