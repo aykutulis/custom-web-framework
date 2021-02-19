8b38dd3338a32811f2c11e96189b6bd1fac224b0
Aykut Ulış <aykutulis@hotmail.com>
2021-02-19 03:09:43 +0300

    Adding model property

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,27 +1,26 @@
+import { User } from './../models/User';
 export class UserForm {
-  constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {}
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      'click:button': this.onButtonClick,
-      'mouseenter:h1': this.onHoverHeading,
+      'click:.set-age': this.onSetAgeClick,
     };
   }
 
-  onButtonClick(): void {
+  onSetAgeClick(): void {
     console.log('Hi there!');
-  }
-
-  onHoverHeading(): void {
-    console.log('Hover on heading!');
   }
 
   template(): string {
     return `
       <div>
         <h1>User Form</h1>
+        <div>User name: ${this.model.get('name')}</div>
+        <div>User age: ${this.model.get('age')}</div>
         <input />
         <button>Click me!</button>
+        <button class="set-age">Set random age</button>
       </div>
     `;
   }
