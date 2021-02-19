24f1e843667d6031f5eb45e2bf932cc02d8cdf62
Aykut Ulış <aykutulis@hotmail.com>
2021-02-19 04:31:26 +0300

    View class generic constraint

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,6 +1,6 @@
-import { User } from './../models/User';
+import { User, UserProps } from './../models/User';
 import { View } from './View';
-export class UserForm extends View {
+export class UserForm extends View<User, UserProps> {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,
